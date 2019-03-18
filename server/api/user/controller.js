b961723c0e93e828fa2bcb29ec5260765ae39697
AARSHI <aarora0301@gmail.com>
2019-03-18 15:27:06 +0800

    add user api

---
+++ server/api/user/controller.js
@@ -0,0 +1,61 @@
+const isEmpty = require('lodash/isEmpty');
+const commonRepository = require('../common').repository;
+const apiConfig = require('../../config').API;
+const passport = require('./passport');
+
+function get(id, limit) {
+  const findOne = Boolean(id);
+  limit = !findOne ? limit || apiConfig.RESPONSE_COUNT_LIMIT : null;
+  const query = findOne ? { _id: id } : {};
+  return commonRepository.getOneOrAll('Users', query, null, limit, findOne);
+}
+
+function createUser(request) {
+  const user = commonRepository.createModel('Users', request.body.user);
+  user.setPassword(request.body.user.password);
+  return user;
+}
+
+function getByEmail(email) {
+  return commonRepository.getOneOrAll('Users', { email }, null, null, true);
+}
+
+async function save(request) {
+  try {
+    const existingUser = await getByEmail(request.body.user.email);
+    const isUserPresent = !isEmpty(existingUser);
+
+    // needs improvment, function should have consistent return type
+    if (isUserPresent) return Promise.reject('User Already exists');
+    return commonRepository.save(null, null, null, (createUser(request)));
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+function sendAuthUser(user) {
+  return user.toAuthJSON();
+}
+
+function getUser(req, res) {
+  return passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      throw err;
+    }
+
+    if (user) {
+      const _user = user;
+      _user.token = user.generateJWT();
+      return res.json({ user: _user.toAuthJSON() }); // needs improvement res to be set in route only
+    }
+    return res.status(400).json(info);
+  })(req, res);
+}
+
+
+module.exports = {
+  sendAuthUser,
+  save,
+  getUser,
+
+};
---
+++ server/api/user/route.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const statusCodes = require('http-status-codes');
+const auth = require('../common').auth;
+const v = require('./validation');
+const {check, validationResult} = require('express-validator/check');
+const controller = require('./controller');
+
+
+// we might need to access all the routing params from parent as well,
+// so the better practice is to have mergeParams: true
+const router = express.Router({mergeParams: true});
+
+function post(req, res) {
+    const errors = validationResult(req).formatWith(v.errorFormatter);
+
+    if (!errors.isEmpty()) {
+        res.status(400).json(errors.array());
+    } else {
+        const result = controller.save(req);
+        result
+            .then((user) => {
+                res.json({user: controller.sendAuthUser(user)});
+            }).catch((err) => {
+            res.status(statusCodes.INTERNAL_SERVER_ERROR).send(err);
+        });
+    }
+}
+
+function get(req, res) {
+    const errors = validationResult(req, res).formatWith(v.errorFormatter);
+    if (!errors.isEmpty()) {
+        res.status(400).json(errors.array());
+    } else {
+        try {
+            controller.getUser(req, res);
+        } catch (err) {
+            res.status(statusCodes.INTERNAL_SERVER_ERROR).send(err);
+        }
+    }
+}
+
+router.post('/', auth.optional, v.requestValidator, post);
+router.post('/login', auth.optional, v.requestValidator, get);
+module.exports = router;
